138743837651fde6bba51572ac6448dbf0c88d84
User <sen.hakobyan@gmail.com>
2020-04-18 19:39:46 +0400

    handling router active state

--- resources/js/components/Doctor/PatientList.vue
+++ resources/js/components/Doctor/PatientList.vue
@@ -1,16 +1,14 @@
 <template>
     <div class="col-3">
         <h3>Patient List
-            <RouterLink
-                to="/add-patient"
-            >
+            <RouterLink to="/add-patient">
                 <button class="btn btn-outline-success btn-sm mb-3 float-right">Add New Patient <i class="fa fa-plus"></i></button>
             </RouterLink>
         </h3>
         <div class="clearfix"></div>
         <ul v-if="patients.length" class="list-group list-unstyled">
             <li v-bind:key="patient.id" v-for="patient in patients" class="list-group-item list-group-item-action cursor-pointer p-0">
-                <RouterLink :to="'/patient/'+patient.id" class="d-block p-2 text-secondary">
+                <RouterLink :to="'/patient/'+patient.id" class="d-block p-2 text-secondary" active-class="active">
                     {{ patient.name }}
                 </RouterLink>
             </li>
