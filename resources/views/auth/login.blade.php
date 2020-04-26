4c053cce4af82aa78b6046c151c88b83cfeef59e
User <sen.hakobyan@gmail.com>
2020-04-26 20:39:53 +0400

    colors changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header bg-success text-white">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -53,8 +53,8 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-danger">
-                                    {{ __('Login') }}
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa fa-sign-in"></i> {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header bg-success text-white">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -83,8 +83,8 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-danger">
-                                    {{ __('Register') }}
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa fa-user"></i> {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
